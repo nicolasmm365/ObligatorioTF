41e0fd0922da2e0b218bb3bed70633e6dfcb757c
nicolasmm365 <162520612+nicolasmm365@users.noreply.github.com>
2024-06-22 22:39:35 -0300

    Se agregan nuevas variables.

--- main.tf
+++ main.tf
@@ -4,8 +4,17 @@ provider "aws" {
 
 module "vpc" {
   source       = "./modules/vpc"
-  vpc_cidr     = var.vpc_cidr
   subnet_cidrs = var.subnet_cidrs
+  vpc_cidr        = var.vpc_cidr             # Direccion de Red
+#  subnet_a_cidr   = var.subnet_a_cidr        # Subnet a
+#  subnet_b_cidr   = var.subnet_b_cidr        # Subnet b
+  vpc_aws_az-a    = var.vpc_aws_az-a         # AZ a
+  vpc_aws_az-b    = var.vpc_aws_az-b         # AZ b
+  vpc_name        = var.vpc_name             # Nombre del VPC
+  tag_subnet_a    = var.tag_subnet_a         # Tag Subnet a
+  tag_subnet_b    = var.tag_subnet_b         # Tag Subnet b
+  tag_igw         = var.tag_igw              # Tag IGW
+  tag_rtable      = var.tag_rtable           # Tag Route Table
 }
 
 module "rds" {
@@ -42,6 +51,17 @@ module "ec2" {
   db_password  = var.db_password
   db_name      = module.rds.db_name
   key_name     = var.key_name
+#  id_vpc            = module.vpc.id_vpc          # Variable ID de la VPC
+#  id_subnet_a       = module.vpc.id_subnet_a     # Variable ID de la Subnet a
+#  id_subnet_b       = module.vpc.id_subnet_b     # Variable ID de la Subnet b
+  nombre_sg_lb      = var.nombre_sg_lb           # Nombre del SG del LB
+  tag_sg_lb         = var.tag_sg_lb              # Tag del SG del LB
+  nombre_sg_appweb  = var.nombre_sg_appweb       # Nombre del SG del Servidor web 
+  tag_sg_appweb     = var.tag_sg_appweb          # Tag del SG del Servidor web
+  nombre_sg_mysql   = var.nombre_sg_mysql        # Nombre del SG de MySql 
+  tag_sg_mysql      = var.tag_sg_mysql           # Tag del SG de MySql
+  nombre_sg_efs     = var.nombre_sg_efs          # Nombre del SG del EFS 
+  tag_sg_efs        = var.tag_sg_efs             # Tag del SG del EFS 
 
   depends_on = [ module.vpc, module.efs, module.rds ]
 }
