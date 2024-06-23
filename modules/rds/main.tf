6e4a21211411859dc910e11d29a6d4712cb3b2a4
Alexis D'Andrea <56324802+adandrea8@users.noreply.github.com>
2024-06-22 22:47:40 -0300

    se agregan variables al main de rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,18 +7,18 @@ resource "aws_db_subnet_group" "obligatorio_db_subnet_group" {
 }
 
 resource "aws_db_instance" "obligatorio-db" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7.44"
-  instance_class       = "db.t3.micro"
+  allocated_storage    = var.allocated_storage
+  storage_type         = var.storage_type
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
   username             = var.db_username
   password             = var.db_password
   skip_final_snapshot  = true
   vpc_security_group_ids = [var.db_sg_id]
   db_subnet_group_name = aws_db_subnet_group.obligatorio_db_subnet_group.name
-  db_name              = "iDukan"
+  db_name              = var.db_name
   tags = {
-    Name = "obligatorio-db"
+    Name = var.tag_name_db
   }
 }
