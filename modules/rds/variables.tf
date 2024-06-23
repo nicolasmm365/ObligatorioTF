9a47bc8e871cfb8869d35ebf8f80fe9639dcc52e
Alexis D'Andrea <56324802+adandrea8@users.noreply.github.com>
2024-06-22 22:47:57 -0300

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -3,3 +3,11 @@ variable "subnet_ids" {}
 variable "db_sg_id" {}
 variable "db_username" {}
 variable "db_password" {}
+
+variable "instance_class" {}
+variable "tag_name_db" {}
+variable "db_name" {}
+variable "engine_version" {}
+variable "engine" {}
+variable "storage_type" {}
+variable "allocated_storage" {}
