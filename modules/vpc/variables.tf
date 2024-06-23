d651cc9ac36a4a33e1baebd589c9a925ad38cf6b
nicolasmm365 <162520612+nicolasmm365@users.noreply.github.com>
2024-06-23 14:59:16 -0300

    Se mejora formato de variables.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,30 +1,25 @@
+# VPC
 variable "vpc_cidr" {
-  #default = "10.0.0.0/16"
   description = "Direccion y mascara"
 }
 
 variable "subnet_a_cidr" {
-  #default = "10.0.1.0/24"
   description = "Direccion de primer subred"
 }
 
 variable "subnet_b_cidr" {
-  #default = "10.0.2.0/24"
   description = "Direccion segunda subred"
 }
 
 variable "vpc_aws_az-a" {
-  #default = "us-east-1a"
   description = "Zona de Disponibilidad a"
 }
 
 variable "vpc_aws_az-b" {
-  #default = "us-east-1b"
   description = "Zona de Disponibilidad b"
 }
 
 variable "vpc_name" {
-  #default = "vpc-obligatorio"
   description = "Nombre del VPC"
 }
 
