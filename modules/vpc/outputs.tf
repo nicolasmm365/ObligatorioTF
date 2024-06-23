9cf19531b999c5421ee2656aa50c2a8c1ba85206
Alexis D'Andrea <56324802+adandrea8@users.noreply.github.com>
2024-06-23 15:03:13 -0300

    Se agregan las descripciones a todos los outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,20 +1,25 @@
 output "id_vpc" {
+  description = "ID del VPC"
   value = aws_vpc.vpc_obligatorio.id
 }
 
 output "id_subnet_a" {
+  description = "ID de la Subnet A"
   value = aws_subnet.subnet_a.id
 }
 
 output "id_subnet_b" {
+  description = "ID de la Subnet B"
   value = aws_subnet.subnet_b.id
 }
 
 output "subnet_a_cidr" {
+  description = "CIDR de la Subnet A"
   value = aws_subnet.subnet_a.cidr_block
 }
 
 output "subnet_b_cidr" {
+  description = "CIDR de la Subnet B"
   value = aws_subnet.subnet_b.cidr_block
 }
 
