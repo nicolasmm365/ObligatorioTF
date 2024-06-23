dd3d6d557be66690d53680714ecd7436a6f03e1d
nicolasmm365 <162520612+nicolasmm365@users.noreply.github.com>
2024-06-23 14:56:56 -0300

    Se mejora formato de variables.

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,14 +1,53 @@
+# Security Group
+variable "db_sg_id" {
+  description = "ID del Security Group de base de datos"
+}
 
-variable "db_sg_id" {}
-variable "rds_db_username" {}
-variable "rds_db_password" {}
-variable "id_subnet_a" {}
-variable "id_subnet_b" {}
+# VPC
+variable "id_subnet_a" {
+  description = "ID del Security Group de subnet a"
+}
 
-variable "rds_instance_class" {}
-variable "rds_tag_name_db" {}
-variable "rds_db_name" {}
-variable "rds_engine_version" {}
-variable "rds_engine" {}
-variable "rds_storage_type" {}
-variable "rds_allocated_storage" {}
+variable "id_subnet_b" {
+  description = "ID del Security Group de subnet b"
+}
+
+# RDS
+variable "rds_instance_class" {
+  description = "Tipo de instancia"
+}
+
+variable "rds_tag_name_db" {
+  description = "Tag de base de datos"
+}
+
+variable "db_name" {
+  description = "Nombre de base de datos"
+}
+
+variable "rds_engine_version" {
+  description = "Version de motor de base de datos"
+}
+
+variable "rds_engine" {
+  description = "Motor de base de datos"
+}
+
+variable "rds_storage_type" {
+  description = "Tipo de storate"
+}
+
+variable "rds_allocated_storage" {
+  description = "Localidad del storage"
+}
+
+variable "rds_db_username" {
+  description = "Usuario de la instancia RDS"
+  type        = string
+}
+
+variable "rds_db_password" {
+  description = "Password de la instancia RDS"
+  type        = string
+  sensitive   = true
+}
